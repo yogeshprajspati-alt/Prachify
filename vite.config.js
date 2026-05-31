d816c2ccc57ad23b7921b39f393b3c72103c577e
Your Name <your.email@example.com>
2026-05-31 12:51:10 +0530

    icon issue fixed

--- vite.config.js
+++ vite.config.js
@@ -33,7 +33,7 @@ export default defineConfig({
         ],
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        globPatterns: ['**/*.{js,css,html,png,svg,woff2}'],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
